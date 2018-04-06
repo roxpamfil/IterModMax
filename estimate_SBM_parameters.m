cd0d4329da27e1d4b2214647b0e79b9a06dba305
Roxana Feier <feier@maths.ox.ac.uk>
2018-04-06 14:40:58 +0100

    added functionality for bipartite networks

--- estimate_SBM_parameters.m
+++ estimate_SBM_parameters.m
@@ -8,25 +8,32 @@
 %   - S is an N x T matrix of community labels, where N is the number of
 %     nodes
 %
+% Optional inputs:
+%   - uniform: set to 1 (default) if the parameters are uniform across layers, 
+%     and to 0 otherwise
+%   - networktype: set to 'unipartite' (default) or 'bipartite', depending on
+%     the structure of the network 
+%
 % Outputs:
 %   - th_in and th_out, which correspond to the intra-community and
 %     inter-community edge probabilities (note that the model assumes
-%     degree correction); note that these parameters are global estimates,
-%     for all the layers
+%     degree correction)
 %   - p, which is the probability that a node copies its community label
-%     from the previous layer in a temporal network; also estimated
-%     globally
+%     from the previous layer in a temporal network
 %   - K, which is the number of communities
 %
 % This function calls a number of helper functions below.
 %
-function [th_in, th_out, p, K] = estimate_SBM_parameters(A, S, uniform)
+function [th_in, th_out, p, K] = estimate_SBM_parameters(A, S, uniform, networktype)
   if nargin < 3
     uniform = 1;
   end
+  if nargin < 4
+    networktype = 'unipartite';
+  end
   
   if uniform
-    [th_in, th_out] = estimate_th_in_th_out(A, S);
+    [th_in, th_out] = estimate_th_in_th_out(A, S, networktype);
     K = estimate_K(S);
     p = estimate_p(S, K);
   else
@@ -37,7 +44,7 @@ function [th_in, th_out, p, K] = estimate_SBM_parameters(A, S, uniform)
     K = zeros(T, 1);
     
     for t=1:T
-      [th_in_t, th_out_t] = estimate_th_in_th_out({A{t}}, S(:, t));
+      [th_in_t, th_out_t] = estimate_th_in_th_out({A{t}}, S(:, t), networktype);
       th_in(t) = th_in_t;
       th_out(t) = th_out_t;
       K(t) = estimate_K(S(:, t));
@@ -49,35 +56,71 @@ function [th_in, th_out, p, K] = estimate_SBM_parameters(A, S, uniform)
   end
 end
 
-% TODO: input twom and degrees so as not to re-calculate each time
-function [th_in, th_out] = estimate_th_in_th_out(A, S)
+% Estimate edge probabilities th_in and th_out
+function [th_in, th_out] = estimate_th_in_th_out(A, S, networktype)
   T = length(A);
+  [M, N] = size(A{1});
   
-  % theta_in and theta_out assuming degree-corrected model
-  num_in = 0; denom_in = 0;
-  num_out = 0; denom_out = 0;
-  for t = 1:T
-    twom = nnz(A{t});
-    deg = sum(A{t}, 1);
-    deg_r = zeros(max(S(:, t)), 1);
-    twom_in = 0;
+  if ~strcmp(networktype, 'bipartite')
+    % Unipartite network
     
-    % sum of degrees in each group, and number of within-community edges
-    for r = 1:max(S(:,t))
-      idx = S(:, t) == r;
-      deg_r(r) = sum(deg(idx));
-      twom_in = twom_in + nnz(A{t}(idx, idx));
+    % theta_in and theta_out assuming degree-corrected model
+    num_in = 0; denom_in = 0;
+    num_out = 0; denom_out = 0;
+    for t = 1:T
+      twom = nnz(A{t});
+      deg = sum(A{t}, 1);
+      deg_r = zeros(max(S(:, t)), 1);
+      twom_in = 0;
+
+      % sum of degrees in each group, and number of within-community edges
+      for r = 1:max(S(:,t))
+        idx = S(:, t) == r;
+        deg_r(r) = sum(deg(idx));
+        twom_in = twom_in + nnz(A{t}(idx, idx));
+      end
+
+      num_in = num_in + twom_in;
+      num_out = num_out + twom - twom_in;
+      term = sum(deg_r .^ 2) / twom;
+      denom_in = denom_in + term;
+      denom_out = denom_out + twom - term;
     end
- 
-    num_in = num_in + twom_in;
-    num_out = num_out + twom - twom_in;
-    term = sum(deg_r .^ 2) / twom;
-    denom_in = denom_in + term;
-    denom_out = denom_out + twom - term;
+    th_in = num_in / denom_in;
+    th_out = num_out / denom_out;
+  else
+    % Bipartite network
+    
+    % theta_in and theta_out assuming degree-corrected model
+    num_in = 0; denom_in = 0;
+    num_out = 0; denom_out = 0;
+    for t = 1:T
+      mm = nnz(A{t});
+      deg_rows = sum(A{t}, 2);
+      deg_cols = sum(A{t}, 1);
+      deg_r1 = zeros(max(S(:, t)), 1);
+      deg_r2 = zeros(max(S(:, t)), 1);
+      m_in = 0;
+
+      % sum of degrees in each group, and number of within-community edges
+      for r = 1:max(S(:,t))
+        idx = S(:, t) == r;
+        idx1 = idx(1:M);
+        idx2 = idx(M+1:M+N);
+        deg_r1(r) = sum(deg_rows(idx1));
+        deg_r2(r) = sum(deg_cols(idx2));
+        m_in = m_in + nnz(A{t}(idx1, idx2));
+      end
+
+      num_in = num_in + m_in;
+      num_out = num_out + mm - m_in;
+      term = sum(deg_r1 .* deg_r2) / mm;
+      denom_in = denom_in + term;
+      denom_out = denom_out + mm - term;
+    end
+    th_in = num_in / denom_in;
+    th_out = num_out / denom_out;
   end
-  
-  th_in = num_in / denom_in;
-  th_out = num_out / denom_out;
 end
 
 function [p] = estimate_p(S, K)
@@ -90,6 +133,6 @@ function [p] = estimate_p(S, K)
 end
 
 function [K] = estimate_K(S)
-  %K = length(unique(S(:)));
-  K = max(S(:));
+  K = length(unique(S(:)));
+  %K = max(S(:));
 end
--- it_mod_max_temporal.m
+++ it_mod_max_temporal.m
@@ -18,13 +18,15 @@
 %     smaller communities being detected
 %   - uniform: set to 1 if the parameters are uniform across layers, and to
 %     0 otherwise; default value uniform = 1
-%   - algtype: set to one of '' (standard GenLouvain), 'it' (iterated
+%   - algtype: set to one of 'st' (standard GenLouvain), 'it' (iterated
 %     GenLouvain), or 'itPP' (iterated GenLouvain with post-processing);
 %     default value algtype = 'itPP'
 %   - movetype: set to one of 'move', 'moverand', or 'moverandw' (see
 %     GenLouvain options); default value movetype = 'moverandw'
 %   - drawflag: set to 1 to show a drip plot of the multilayer community
 %     structure at each step of the iteration; default value drawflag = 0
+%   - networktype: one of 'undirected' (default), 'directed', or 'bipartite'
+%     (which implicitly assumes undirected edges)
 %
 % Outputs:
 %   - gamma, omega, beta: parameter estimates at convergence, or parameter
@@ -41,20 +43,20 @@
 %   - by default, convergence tolerance is 1e-2 for gamma and 5e-2 for
 %     omega; the larger tolerance for the latter is justified by the fact 
 %     that modularity maximisation is less sensitive to the choice of omega
-%   - max_iter is the maximum number of iterations, set to 8 by default
+%   - max_iter is the maximum number of iterations, set to 20 by default
 %   - when the algorithm fails to converge in max_iter iterations, it 
 %     returns the multilayer partition S with the highest value of the 
 %     normalised modularity Q
 % 
 % Dependencies:
 %   - genlouvain.m and iterated_genlouvain.m, which need to be in a 
-%     directory "../GenLouvain2.1" (change if necessary)
+%     directory "../GenLouvain2.1" or otherwise in the Matlab path
 %   - estimate_SBM_parameters.m, optimal_gamma.m, optimal_omega.m,
 %     optimal_beta.m, which need to be in the same directory as this file
 %
 function [gamma, omega, beta, S, Q, converged] = ...
   it_mod_max_temporal(A, gamma0, omega0, beta0, K_max, ...
-  uniform, algtype, movetype, drawflag)
+  uniform, algtype, movetype, drawflag, networktype)
 
   % Add path to GenLouvain code
   addpath(genpath('../GenLouvain2.1/'));
@@ -79,17 +81,23 @@ function [gamma, omega, beta, S, Q, converged] = ...
   if nargin < 9 || isempty(drawflag)
     drawflag = 0;
   end
+  if nargin < 10 || isempty(networktype)
+    networktype = 'undirected';
+  end
 
   % Process inputs
   T = length(A);  % number of layers
   N = length(A{1});  % number of nodes in each layer
+  if strcmp(networktype, 'bipartite')
+    N = sum(size(A{1}));
+  end
   PP = @(S) postprocess_ordinal_multilayer(S, T);  % post-processing function
 
   gamma = gamma0; omega = omega0; beta = beta0;
   
   % Convergence settings, change if desired
-  tol = 1e-2;  % convergence tolerance
-  max_iter = 8;  % 
+  tol = 1e-2;     % convergence tolerance
+  max_iter = 20;  % maximum number of iterations 
   converged = 0;
   iter = 0;
   
@@ -103,8 +111,18 @@ function [gamma, omega, beta, S, Q, converged] = ...
       iter = iter + 1;
       
       % Run multilayer modularity with current values of omega and gamma
-      [B, twom] = multiord(A, gamma, omega);  % ordinal interlayer coupling
-      if strcmp(algtype, '')
+      
+      % Construct modularity matrix B
+      if strcmp(networktype, 'undirected')
+        [B, twom] = multiord(A, gamma, omega);  
+      elseif strcmp(networktype, 'directed')
+        [B, twom] = multiorddir_f(A, gamma, omega);
+      elseif strcmp(networktype, 'bipartite')
+        [B, twom] = multiordbipartite(A, gamma, omega);
+      end
+      
+      % Run GenLouvain algorithm
+      if strcmp(algtype, 'st')
         [S, Q] = genlouvain(B, [], 0, 1, movetype);
       elseif strcmp(algtype, 'it')
         [S, Q, ~] = iterated_genlouvain(B, [], [], 1, movetype);
@@ -162,11 +180,6 @@ function [gamma, omega, beta, S, Q, converged] = ...
         converged = 1;
       end
       
-      % Keep iterating if there is chance of convergence
-      %if iter == max_iter && K <= 5
-      %  max_iter = min(max_iter + 8, 16);
-      %end
-      
       gamma = gamma_new; omega = omega_new;
       
     end
@@ -194,12 +207,12 @@ function [gamma, omega, beta, S, Q, converged] = ...
     
     Q_best = 0;
     
-    while ~converged
+    while ~converged && iter < max_iter
       iter = iter + 1;
 
       % Run multilayer modularity with current values of omega and gamma
       [B, twom] = multiordgen(A, gamma, omega, beta); 
-      if strcmp(algtype, '')
+      if strcmp(algtype, 'st')
         [S, Q] = genlouvain(B, [], 0, 1, movetype);
       elseif strcmp(algtype, 'it')
         [S, Q, ~] = iterated_genlouvain(B, [], [], 1, movetype);
